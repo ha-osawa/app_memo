b94ead832874cd490032c2b3d4aa3c8f2960bc78
h-osawa <h-osawa@ruby-dev.jp>
2023-06-01 15:48:19 +0900

    メモアプリの修正1

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
 
-json_path = 'public/memo_data.json'
+json_path = 'data/memo_data.json'
 
 helpers do
   def h(text)
@@ -13,63 +13,59 @@ helpers do
   end
 end
 
-get '/' do
-  @title = 'Top'
-  @css_link = '/css/top.css'
-  @memos =  File.open(json_path) { |file| JSON.parse(file.read) } unless File.empty?(json_path)
+get '/memos' do
+  @title = 'メモ一覧'
+  @memos = File.open(json_path) { |file| JSON.parse(file.read) } unless File.empty?(json_path)
   erb :top
 end
 
-get '/memo/new' do
-  @title = 'New memo'
-  @css_link = '/css/edit_and_new.css'
+get '/memos/new' do
+  @title = '新規作成'
   erb :new
 end
 
-post '/memo/new' do
-  memo_hash = []
-  File.open(json_path, 'a+') do |file|
-    memo_hash = JSON.parse(file.read) unless File.empty?(json_path)
-    memo_hash.push({ "id": SecureRandom.uuid, "title": h(params[:memo_title]), "content": h(params[:memo_content]) })
+post '/memos/new' do
+  memos = []
+  File.open(json_path, 'r') do |file|
+    memos = JSON.parse(file.read) unless File.empty?(json_path)
+    memos.push({ id: SecureRandom.uuid, title: params[:memo_title], content: params[:memo_content] })
   end
 
-  File.open(json_path, 'w+') do |file|
-    JSON.dump(memo_hash, file)
-  end
-  redirect '/'
-end
-
-get '/memo/:memo_id' do
-  memos = File.open(json_path) { |file| JSON.parse(file.read) }
-  @memo = memos.find { |memo| memo['id'].include?(h(params[:memo_id])) }
-  @title = 'Show memo'
-  @css_link = '/css/show.css'
-  erb :show
-end
-
-delete '/memo/:memo_id' do
-  memos = File.open(json_path) { |file| JSON.parse(file.read) }
-  remain_memo = memos.delete_if { |memo| memo['id'].include?(h(params[:memo_id])) }
-  File.open(json_path, 'w') do |file|
-    JSON.dump(remain_memo, file)
-  end
-  redirect '/'
-end
-
-get '/memo/:memo_id/edit' do
-  @title = 'Edit memo'
-  @memo_id = h(params[:memo_id])
-  @css_link = '/css/edit_and_new.css'
-  erb :edit
-end
-
-patch '/memo/:memo_id' do
-  memos = File.open(json_path) { |file| JSON.parse(file.read) }
-  memo_index = memos.find_index { |memo| memo['id'].include?(h(params[:memo_id])) }
-  memos[memo_index]['title'] = h(params[:memo_title]) unless h(params[:memo_title]).empty?
-  memos[memo_index]['content'] = h(params[:memo_content]) unless h(params[:memo_content]).empty?
   File.open(json_path, 'w') do |file|
     JSON.dump(memos, file)
   end
-  redirect "/memo/#{h(params[:memo_id])}"
+  redirect '/memos'
+end
+
+get '/memos/:memo_id' do
+  memos = File.open(json_path) { |file| JSON.parse(file.read) }
+  @memo = memos.find { |memo| memo['id'].eql?(params[:memo_id]) }
+  @title = 'メモの詳細'
+  erb :show
+end
+
+delete '/memos/:memo_id' do
+  memos = File.open(json_path) { |file| JSON.parse(file.read) }
+  remain_memos = memos.delete_if { |memo| memo['id'].eql?(params[:memo_id]) }
+  File.open(json_path, 'w') do |file|
+    JSON.dump(remain_memos, file)
+  end
+  redirect '/memos'
+end
+
+get '/memos/:memo_id/edits' do
+  @title = 'メモの編集'
+  @memo_id = params[:memo_id]
+  erb :edit
+end
+
+patch '/memos/:memo_id' do
+  memos = File.open(json_path) { |file| JSON.parse(file.read) }
+  memo_index = memos.find_index { |memo| memo['id'].eql?(params[:memo_id]) }
+  memos[memo_index]['title'] = params[:memo_title] unless params[:memo_title].empty?
+  memos[memo_index]['content'] = params[:memo_content] unless params[:memo_content].empty?
+  File.open(json_path, 'w') do |file|
+    JSON.dump(memos, file)
+  end
+  redirect '/memos'
 end
